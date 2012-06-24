d90e9271d271b6d18f64ff388f36332ea3d252a6
guillermooo <guilan70@hotmail.com>
2012-06-24 13:09:20 +0200

    add tests

--- Tests/powertests/powertests.ps1
+++ Tests/powertests/powertests.ps1
@@ -3,147 +3,308 @@
 
 . "$(split-path $MyInvocation.MyCommand.Path -parent)/misc.ps1"
 
+set-variable TEST_STATUS_ERROR  -value -1 -option "CONSTANT"
+set-variable TEST_STATUS_FAILED -value 0 -option "CONSTANT"
+set-variable TEST_STATUS_PASSED -value 1 -option "CONSTANT"
+set-variable STOPWATCH -value (new-object "system.diagnostics.stopwatch") -option "CONSTANT"
 
 
-$script:failedTests = @()
-$script:totalTests = 0
+# Create a new test result object.
+function New_TestResult {
+    param([int]$Status,
+          [string]$Source,
+          # TODO: we should accept an error field too for tests throwing errors.
+          # $TestCase should then become a [system.management.automation.psvariable]
+          [object]$TestCase,
+          [timespan]$TimeElapsed)
+
+    function AddNoteProperty {
+        param($Target, $Name, $Value)
+        $target | add-member -membertype "NoteProperty" `
+                             -name       $Name `
+                             -value      $Value
+    }
+
+    $o = new-object "PSObject"
+    AddNoteProperty $o "Status" $Status
+    AddNoteProperty $o "Source" $Source
+    AddNoteProperty $o "TestCase" $TestCase
+    AddNoteProperty $o "TimeElapsed" $TimeElapsed
+    $o
+}
 
 
+# Functions for test definition
+#==============================================================================
 function getTestVariables {
-    param([string]$LikePattern, $Scope)
+    param([string]$NamePattern, $Scope)
     # Gets test cases in the parent scope.
     $testCases = (get-variable -scope $Scope) | where-object {
                                                         $_.value -is [scriptblock] -and
-                                                        $_.name -like $LikePattern
+                                                        $_.name -like $NamePattern
                                                         }
     $testCases | sort-object -property @{Expression={ $_.value.startposition.start }}
 }
 
 
-function makeTestSuite { 
-    getTestVariables "SetUp" -scope 2 -erroraction silentlycontinue
-    getTestVariables "TestCase_*" -scope 2
-    getTestVariables "TearDown" -scope 2 -erroraction silentlycontinue
+function makeTestSuite {
+    $testEnvironment = getTestVariables "setUpTestSuite" -scope 2
+    $testCases = getTestVariables "TestCase*" -scope 2
+    $testEnvironment, @($testCases)
 }
-function makeTestCase { getTestVariables "test_*" -scope 2 }
+
+
+function makeTestCase {
+    $testCaseEnvironment = getTestVariables "setUpTestCase" -scope 2
+    $testCaseTests = getTestVariables "test*" -scope 2
+    $testCaseEnvironment, $testCaseTests
+}
 
 
 function doesTestPass {
     param([management.automation.psvariable]$Test)
-    all (& $Test.value)
+    $values = & $Test.value
+    ([bool]@($values).length) -and ((@($values) -eq $false).length -eq 0)
+}
+#==============================================================================
+
+
+# Functions for test result formatting
+#==============================================================================
+function FormatFailedTests {
+    param($failedTests)
+
+    foreach ($fail in $failedTests) {
+        write-host -foreground "DarkYellow" ("FAILED: {0}{1}[{2}:{3}]" -f
+                                                $fail.source,
+                                                $fail.testcase.Name,
+                                                $fail.testcase.value.startposition.startline,
+                                                $fail.testcase.value.startposition.startcolumn)
+        write-host $fail.testcase.value
+    }
 }
 
+function FormatTestsWithErrors {
+    # in powershell, there seems to be two types of error info objects:
+    # - exceptions (they have an ErrorRecord property)
+    # - ErrorRecord
+    # We'll try to always use the ErrorRecord member to extract info.
+    param($errorTests)
 
-function absToRelPath {
-    param($Path)
-    $cd = (resolve-path ($pwd)).providerpath
-    $t = (resolve-path ($path)).providerpath
-    if ($t.startswith($cd)) {
-        join-path '.' ($t[$cd.length..$t.length] -join '')
-    }
-    else {
-        $t
-    }
-}
+    foreach($err in $errorTests) {
 
-
-function printErrors {
-    foreach ($err in $failedTests){
-        write-host ('=' * 80) -foreground DarkGray
-
-        $testSuite, $test = $err["testSuite"], $err["test"]
-        "[{0}]::{1}:{2}:{3}:{4}" -f (
-                                    (absToRelPath $testSuite.value.file),
-                                    $testSuite.name,
-                                    $test.name,
-                                    $test.value.startposition.startline,
-                                    $test.value.startposition.startcolumn
-                                )
-
-        write-host ('-' * 80) -foreground DarkGray
-
-        if (-not ($err["error"])) {
-            $err["test"].value
+        $errRecord = $err.testcase
+        if ($err.testcase -isnot [system.management.automation.errorrecord]) {
+            $errRecord = $err.testcase.ErrorRecord
         }
-        else {
-            write-host -foreground red $err["error"]
+
+        write-host -foreground "DarkRed" ("ERROR: {0}[{1}:{2}]" -f
+                                                $errRecord.invocationinfo.scriptname,
+                                                $errRecord.invocationinfo.scriptlinenumber,
+                                                $errRecord.invocationinfo.offsetinline)
+        write-host $err.testcase
+        write-host $errRecord.invocationinfo.line
+    }
+}
+
+function Format-TestResult {
+    param([System.Object[]]$TestResults,
+          [switch]$NoNewLineAtEndOfTest=$True,
+          [switch]$NoNewLineAtEnd
+    )
+
+    $failedTests = @()
+    $errorTests = @()
+    [timespan]$totalTime = 0
+
+    write-host ("`n" + "-"*78) -ForegroundColor "DarkGray" -NoNewline
+
+    # In PS, $null | %{ "foo" } will print "foo". Make sure we don't try
+    # to operate on $null.
+    if ($testResults -eq $null) {
+        $testResults = @()
+    }
+
+    foreach ($result in $TestResults) {
+        $totalTime += $result.timeelapsed
+        switch ($result.status) {
+             $TEST_STATUS_ERROR  { $errorTests  += ,$result; break }
+             $TEST_STATUS_FAILED { $failedTests += ,$result; break }
+             $TEST_STATUS_PASSED { break }
+             default {
+                write-debug "Formatting (default): $($result.status)"
+                write-host -foreground red "You shouldn't be seeing this." }
+        }
+
+        if (-not $NoNewLineAtEndOfTest) {
+            write-host
         }
     }
 
-    write-host ('=' * 80) -foreground DarkGray
-    write-host -foreground yellow "  $script:totalTests Test(s)" -nonew
-    write-host -foreground darkgray " | " -nonew
-    $errCountColor = if ($script:failedTests.count -gt 0) { "Red" } else { "DarkGreen" }
-    write-host -foreground $errCountColor "$($failedTests.count) Error(s)" -nonew
-    write-host -foreground darkgray " | " -nonew
-    write-host -foreground gray "Time: $($args[0].elapsedmilliseconds / 1000)s"
-    write-host ('=' * 80) -foreground DarkGray
+    if (-not $NoNewLineAtEnd) {
+        write-host
+    }
+
+    if ($failedTests -or $errorTests) {
+        write-debug "Failed Tests: $failedTests"
+        write-debug "Error Tests: $errorTests"
+
+        FormatFailedTests $failedTests
+        FormatTestsWithErrors $errorTests
+    }
+
+    write-host ("-"*78) -ForegroundColor "DarkGray"
+    write-host "Tests: $($TestResults.length) | " -NoNewline
+    write-host "Failed: $($failedTests.length) | " -NoNewline
+    write-host "Errors: $($errorTests.length) | " -NoNewline
+    write-host "Time: $($totalTime)" -ForegroundColor "Gray"
+    write-host ("="*78) -ForegroundColor "DarkGray"
+
 }
 
 
+# Functions for test running
+#==============================================================================
 function Invoke-TestSuite {
-    param([management.automation.psvariable]$TestSuite)
+    param($SourceFileName,
+          $TestSuite,
+          $Environ=$null
+    )
 
-    $tests = & $TestSuite.value
-    foreach ($t in @($tests)) {
-        try {
-            if (doesTestPass $t) {
-                write-host -foreground darkgreen '.' -nonewline
-            }
-            else {
-                write-host -foreground red 'F' -nonewline
-                $script:failedTests += @{ "testSuite"=$testSuite; "test"=$t; "error"=$null }
-            }
+    $source = "[$SourceFileName]::$($TestSuite.Name)::"
+    try {
+        $testCaseEnvironment, $tests = & $TestSuite.value
+        $testCaseEnvironment = $testCaseEnvironment.Value
+    }
+    catch {
+        if ($_.FullyQualifiedErrorId -eq "BadExpression") {
+            write-error -message "Can't run test suite. Make sure you are using 'makeTestCase' and 'makeTestSuite' as appropriate."
+            return
         }
-        catch {
-            write-host -foreground red 'E' -nonewline
-            $script:failedTests += @{ "testSuite"=$testSuite; "test"=$t; "error"=$error[0]}
-        }
-        finally {
-            ++$script:totalTests
+        throw $_
+    }
+
+    if (-not $testCaseEnvironment) {
+        $testCaseEnvironment = {
+            param($Logic)
+            & $Logic
         }
     }
-    write-host
+
+    foreach ($t in @($tests)) {
+        $theLogic = {
+            $STOPWATCH.start()
+            try {
+                if (doesTestPass $t) {
+                    Write-Debug "Test Passed: $source - $($t.Name)"
+                    $STOPWATCH.stop()
+                    New_TestResult $TEST_STATUS_PASSED $source $t $STOPWATCH.elapsed
+                    [void] (New-Event -SourceIdentifier "PowerTest.Success")
+                }
+                else {
+                    $STOPWATCH.stop()
+                    Write-Debug "Test Failed: $source - $($t.Name)"
+                    New_TestResult $TEST_STATUS_FAILED $source $t $STOPWATCH.elapsed
+                    [void] (New-Event -SourceIdentifier "PowerTest.Fail")
+                }
+            }
+            catch {
+                $STOPWATCH.stop()
+                Write-Debug "Test Error: $source - $($t.Name) - $_"
+                New_TestResult $TEST_STATUS_ERROR $source $_ $STOPWATCH.elapsed
+                [void] (New-Event -SourceIdentifier "PowerTest.Error")
+            }
+
+        }
+        & $testCaseEnvironment $theLogic
+        $STOPWATCH.reset()
+    }
 }
 
 
-function Invoke-PowerTests {
-    param($Path)
-    begin {
-        # set-strictmode -version '2.0'
-        $userErrorAP = $ErrorActionPreference
-        $ErrorActionPreference = 'Stop'
-        $sw = new-object 'system.diagnostics.stopwatch'
-        $sw.start()
-    }
+function Invoke-PowerTest {
+    param([System.Collections.HashTable]$TestCollection)
     process {
-        foreach ($p in @($Path)) {
-            $testSuite = @(& $p)
-            if ($testSuite[0].name -eq "SetUp") {
-                $setup = $testSuite[0]
-                $testSuite = $testSuite[1..$($testSuite.Length-1)]
+        foreach ($keyValuePair in $TestCollection.GetEnumerator()) {
+
+            if (!$keyValuePair.value) {
+                continue
             }
-            if ($testSuite[-1].name -eq "TearDown") {
-                $teardown = $testSuite[-1]
-                $testSuite = $testSuite[0..$($testSuite.Length-2)]
-            }   
 
-            & { 
-                & $setup.value
-
-                foreach ($ts in $testSuite) {
-                    invoke-testsuite $ts
+            write-debug "Invoking test cases in: $($keyValuePair.Key) ($($keyValuePair.Value.Length))"
+            foreach ($testSuite in $keyValuePair.Value[1])
+            {
+                $testSuiteEnvironment = $keyValuePair.Value[0].Value
+                if (-not $testSuiteEnvironment) {
+                    $testSuiteEnvironment = {
+                        param($Logic)
+                        & $Logic
+                    }
                 }
 
-                & $teardown.value
+                $logic = { Invoke-TestSuite -SourceFileName $keyValuePair.key `
+                                            -TestSuite $testSuite `
+                                            -Environ $keyValuePair.Value[0].Value }
+                & $testSuiteEnvironment $logic
             }
         }
     }
-    end {
-        $sw.stop()
-        printErrors $sw
-        $ErrorActionPreference = $userErrorAP
-        $script:failedTests = @()
-        $script:totalTests = 0
+}
+#==============================================================================
+
+
+# Functions for test collection
+#==============================================================================
+function Get-PowerTest {
+    # TODO: find tests recursively too.
+    param($Path, $Filter="Test*.ps1")
+    write-debug "Collecting tests from: $Path"
+
+    $testCollection = @{}
+    foreach ($file in (Get-ChildItem -Path $Path -Filter $Filter))
+    {
+        write-debug "Collected test matching: $file"
+        $testCollection[$file.name] += @(& $file.fullname)
+    }
+
+    $testCollection
+}
+#==============================================================================
+
+
+# Main program for test discovery
+#==============================================================================
+function Run-Test {
+    param($Path=(Get-Location), $Filter="Test*.ps1")
+
+    try {
+
+        # unload all modules (todo: too drastic?)
+        $__moduleNames = get-module | select-object -expandproperty "name"
+        remove-module (get-module)
+
+        if (!((test-path $path) -and (get-item $path).PSIsContainer)) {
+            write-error -message "Cannot find specified path or it isn't a directory."
+            return
+        }
+
+        # These events allow to write live feedback about test results.
+        [void] (Register-EngineEvent -SourceIdentifier "PowerTest.Success" -Action { Write-Host "." -ForegroundColor "DarkGreen" -NoNewline })
+        [void] (Register-EngineEvent -SourceIdentifier "PowerTest.Fail"    -Action { Write-Host "F" -ForegroundColor "DarkRed" -NoNewline })
+        [void] (Register-EngineEvent -SourceIdentifier "PowerTest.Error"   -Action { Write-Host "E" -ForegroundColor "Red" -NoNewline })
+
+        $testCollection = Get-PowerTest -Path $Path -Filter $Filter
+        if ($testCollection.Count -eq 0) {
+            write-error -message "Could not find any files in '$Path' matching '$Filter'."
+            return
+        }
+
+        $testResults = Invoke-PowerTest -TestCollection $testCollection
+        Format-TestResult -TestResult $testResults
+
+        Unregister-Event -SourceIdentifier "PowerTest.*"
+    }
+    finally {
+        $__moduleNames | import-module
     }
 }
