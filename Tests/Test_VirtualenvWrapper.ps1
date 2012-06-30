7bafbdd559db08654b2c9956e00269b0dcb40858
guillermooo <guilan70@hotmail.com>
2012-06-30 16:03:32 +0200

    add tests

--- Tests/Test_VirtualenvWrapper.ps1
+++ Tests/Test_VirtualenvWrapper.ps1
@@ -4,24 +4,86 @@ $setUpTestSuite = {
     $_oldWORKON_HOME = $env:WORKON_HOME
     $_oldVIRTUALENVWRAPPER_HOOK_DIR = $VIRTUALENVWRAPPER_HOOK_DIR
 
-    import-module "./../virtualenvwrapper"
-
     & $logic
 
-    remove-module "virtualenvwrapper"
-
     unregister-event "virtualenvwrapper.*"
-    remove-item function:VEW_RunInSubProcess
 
     remove-item env:WORKON_HOME -erroraction "SilentlyContinue"
-    remove-item variable:VIRTUALENVWRAPPER_HOOK_DIR -erroraction "SilentlyContinue"
     if ($_oldWORKON_HOME) { $env:WORKON_HOME = $_oldWORKON_HOME }
     if ($_oldVIRTUALENVWRAPPER_HOOK_DIR) { $global:VIRTUALENVWRAPPER_HOOK_DIR = $_oldVIRTUALENVWRAPPER_HOOK_DIR }
 }
 
-$TestCase_Foo = {
-    $test_foo = {
-        1 -eq 2
+$TestCase_Initialisation = {
+    $test_WORKON_HOME_VariableIsSetCorrecly = {
+        remove-item env:WORKON_HOME -erroraction "SilentlyContinue"
+        import-module "../virtualenvwrapper"
+
+        $env:WORKON_HOME -eq "$HOME/.virtualenvs"
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_WORKON_HOME_ExistingValueIsRespected = {
+        $env:WORKON_HOME = "$env:TEMP"
+        import-module "../virtualenvwrapper"
+
+        $env:WORKON_HOME -eq "$env:TEMP"
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_PYTHON_IsSetCorrectly = {
+        remove-item variable:VIRTUALENVWRAPPER_PYTHON
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_PYTHON -eq @(get-command "python.exe")[0].definition
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_PYTHON_ExistingValueIsRespected = {
+        $VIRTUALENVWRAPPER_PYTHON = "foo"
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_PYTHON -eq "foo"
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_VIRTUALENV_IsSetCorrectly = {
+        remove-item variable:VIRTUALENVWRAPPER_VIRTUALENV
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_VIRTUALENV -eq "virtualenv.exe"
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_VIRTUALENV_ExistingValueIsRespected = {
+        $VIRTUALENVWRAPPER_VIRTUALENV = "foo"
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_VIRTUALENV -eq "foo"
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_LOG_DIR_IsSetCorrectly = {
+        remove-item variable:VIRTUALENVWRAPPER_LOG_DIR -erroraction "SilentlyContinue"
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_LOG_DIR -eq $env:WORKON_HOME
+
+        remove-module "virtualenvwrapper"
+    }
+
+    $test_VIRTUALENVWRAPPER_VIRTUALENV_ExistingValueIsRespected = {
+        $VIRTUALENVWRAPPER_VIRTUALENV = "foo"
+        import-module "../virtualenvwrapper"
+
+        $VIRTUALENVWRAPPER_VIRTUALENV -eq "foo"
+
+        remove-module "virtualenvwrapper"
     }
 
     makeTestCase
