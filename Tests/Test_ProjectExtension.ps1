d89bb20cbfb5aab7b88e100b482bf2aa1efb7b43
guillermooo <guilan70@hotmail.com>
2012-07-08 12:17:32 +0200

    fix test

--- Tests/Test_ProjectExtension.ps1
+++ Tests/Test_ProjectExtension.ps1
@@ -410,10 +410,11 @@ $TestCase_Templates = {
 
     $test_NonExistantTemplate = {
         $VirtualenvWrapperTemplates = "$newWorkonHome"
-        $theError = (mkproject -envname "xxx" -templates "foo") 2>&1
+        # Discard error message.
+        [void] (mkproject -envname "xxx" -templates "foo") 2> out-null
 
-        # this is a non-terminating error, so we can't try/catch it.
-        $theError.exception.message -eq "Template 'foo' not found. Not applying."
+         # this is a non-terminating error, so we can't try/catch it.
+         $error[0].exception.message -eq "Template 'foo' not found. Not applying."
     }
 
     $test_Template = {
